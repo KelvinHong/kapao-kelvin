51d55d8a159733aa38e6202c9eeb2e1f986f560d
kelvinhong <kh.boon2@gmail.com>
2025-03-24 16:15:48 +0800

    refactor: exif size with docstrings

--- kapao/dataset.py
+++ kapao/dataset.py
@@ -1,19 +1,31 @@
 
-from PIL import Image, ExifTags
+from PIL import Image
+from typing import Tuple
+
 
 # The key for ExifTags Orientation: 
 # https://github.com/python-pillow/Pillow/blob/bca693bd82ce1dab40a375d101c5292e3a275143/src/PIL/ExifTags.py#L40
-orientation = 0x0112
+ORIENTATION_KEY = 0x0112
 
-def exif_size(img: Image.Image):
-    # Returns exif-corrected PIL size
-    s = img.size  # (width, height)
+def exif_size(img: Image.Image) -> Tuple[int, int]:
+    """Return Exif corrected image size.
+
+    Args:
+        img (Image.Image): Pillow image.
+
+    Returns:
+        Tuple[int, int]: Correct image size in (width, height).
+        
+    References:
+        https://sirv.com/help/articles/rotate-photos-to-be-upright
+    """
+    shape = img.size
     exif = img._getexif()
     if exif is not None and isinstance(exif, dict):
-        rotation = dict(exif.items())[orientation]
+        rotation = dict(exif.items())[ORIENTATION_KEY]
         if rotation == 6:  # rotation 270
-            s = (s[1], s[0])
+            shape = (shape[1], shape[0])
         elif rotation == 8:  # rotation 90
-            s = (s[1], s[0])
+            shape = (shape[1], shape[0])
 
-    return s
+    return shape
